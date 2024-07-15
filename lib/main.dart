44a7518678ccb94a540db67208265a91f5b68371
arnis-prieditis <arnis.prieditis19@gmail.com>
2024-07-15 17:31:47 +0300

    add home page

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,64 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Baktēriju Tinder",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 201, 69, 36)),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.titleLarge!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      fontWeight: FontWeight.bold,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Baktēriju Tinder",
+          style: style,
         ),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Placeholder()));
+            },
+            child: const Text("Find a Match!"),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("Current Matches:"),
+          ),
+          Expanded(
+            child: SizedBox(
+              width: 385,
+              child: ListView(
+                children: const [
+                  Placeholder(),
+                  Placeholder(),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
